249ae00e84a3914527350a88bd068bbc69b88aa6
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-10-23 13:02:48 -0400

    Added inventory fragment to display inventory list

---
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/InventoryFragment.java
@@ -0,0 +1,15 @@
+package com.detroitlabs.icandigit.fragments;
+
+import com.detroitlabs.icandigit.objects.Treasure;
+import java.util.ArrayList;
+
+/**
+ * Created by aditishetty on 10/23/14.
+ */
+public class InventoryFragment {
+
+    private ArrayList<Treasure> mTreasures;
+
+
+
+}
