13610c638cac4d0b8a215851ad0f6b8249a3be18
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-10-23 15:05:09 -0400

    Added menu item

--- app/src/main/java/com/detroitlabs/icandigit/fragments/InventoryFragment.java
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/InventoryFragment.java
@@ -2,6 +2,9 @@ package com.detroitlabs.icandigit.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.detroitlabs.icandigit.R;
 
@@ -14,5 +17,11 @@ public class InventoryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.inventory_title);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        inflater.inflate(R.layout.inventory_fragment, container);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
