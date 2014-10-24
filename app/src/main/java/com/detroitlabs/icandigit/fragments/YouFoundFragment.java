fd07e98b435302623c8db88b5b52a91eb431c215
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-24 14:51:47 -0400

    Renamed DugDialogFragment YouFoundFragment, set up onCreateView

---
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/YouFoundFragment.java
@@ -0,0 +1,30 @@
+package com.detroitlabs.icandigit.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.detroitlabs.icandigit.R;
+
+/**
+ * Created by kyleofori on 10/23/14.
+ */
+public class YouFoundFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.dialog_view, container, false);
+
+        return rootView;
+    }
+
+
+    
+
+}
