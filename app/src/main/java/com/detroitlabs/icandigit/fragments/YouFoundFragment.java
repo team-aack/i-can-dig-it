698821400a0a26db9473d30f01eb70c516f3c68d
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-27 08:33:46 -0400

    Nothing looks right, but got all info into dialog box

--- app/src/main/java/com/detroitlabs/icandigit/fragments/YouFoundFragment.java
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/YouFoundFragment.java
@@ -9,18 +9,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.detroitlabs.icandigit.R;
+import com.detroitlabs.icandigit.services.InventoryService;
 
 /**
  * Created by kyleofori on 10/23/14.
  */
 public class YouFoundFragment extends Fragment {
+
+    private TextView mTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.dialog_view, container, false);
 
+        mTextView = (TextView) rootView.findViewById(R.id.dialog_text_view);
+        mTextView.setText(InventoryService.freshTreasure.getItemType());
+        
         return rootView;
     }
 
