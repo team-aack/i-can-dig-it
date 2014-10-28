e8158c37af6d2b360150b2494caa2fc6dbb1d07b
Aditi Shetty <aditi.shetty@detroitlabs.com>
2014-10-28 16:48:36 -0400

    Added custom adapter to inflate list layout

--- app/src/main/java/com/detroitlabs/icandigit/fragments/InventoryFragment.java
+++ app/src/main/java/com/detroitlabs/icandigit/fragments/InventoryFragment.java
@@ -1,13 +1,16 @@
 package com.detroitlabs.icandigit.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.detroitlabs.icandigit.R;
 import com.detroitlabs.icandigit.objects.Treasure;
@@ -26,7 +29,60 @@ public class InventoryFragment extends Fragment {
         getActivity().setTitle(R.string.inventory_title);
     }
 
-    public class InventoryAdapter extends BaseAdapter {
+    public abstract class InventoryAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+        private ArrayList<Treasure> mTreasure;
+        private Context context;
+
+        public InventoryAdapter(Context context, ArrayList<Treasure> treasure) {
+            this.context = context;
+            mInflater = LayoutInflater.from(context);
+            mTreasure = treasure;
+        }
+
+        @Override
+        public int getCount() {
+            return mTreasure.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = null;
+            ViewHolder holder;
+
+            if(convertView == null) {
+                view = mInflater.inflate(R.layout.inventory_list_row, parent, false);
+                holder = new ViewHolder();
+                holder.icon = (ImageView)view.findViewById(R.id.treasureIcon);
+                holder.multiple = (TextView)view.findViewById(R.id.multipleSign);
+                holder.counter = (TextView)view.findViewById(R.id.treasureCounter);
+                holder.treasure = (TextView)view.findViewById(R.id.treasureItem);
+                view.setTag(holder);
+            } else {
+                view = convertView;
+                holder = (ViewHolder)view.getTag();
+            }
+
+            Treasure treasure = mTreasure.get(position);
+            //holder.icon.setImageBitmap(treasure.getIcon());
+            holder.icon.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_treasure));
+            holder.multiple.setText("X");
+            holder.counter.setText(treasure.getItemAmount());
+            holder.treasure.setText(treasure.getItemType());
+
+            return view;
+        }
+
+        private class ViewHolder {
+            public ImageView icon;
+            public TextView multiple, counter, treasure;
+        }
 
     }
 
